5104c2d5296ab248dccf81c32bde9ebb42078f06
Niklas Rueber <nrueber@igus.net>
2026-03-23 16:57:52 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:EventElevator/EventElevator.Tests/UnitTest1.cs

--- EventElevator/EventElevator.Tests/UnitTest1.cs
+++ EventElevator/EventElevator.Tests/UnitTest1.cs
@@ -41,6 +41,8 @@ public class UnitTest1
         var controller = new ElevatorController();
         var eventAggregator = EventAggregator.GetEventAggregator();
         var actualTargetFloor = 0;
+        
+        // we subscribe to expected event
         eventAggregator.Subscribe(typeof(MoveUpEvent), (theEvent) =>
         {
             actualTargetFloor = theEvent.TargetFloor;
@@ -48,9 +50,13 @@ public class UnitTest1
         var targetFloor = 5;
         var expectedTargetFloor = 5;
         
+        // Tell the controller where we are
         controller.TellCurrentFloor(0);
+        
+        // Tell the controller where we want to go (floor 5)
         controller.EvaluateDirection(targetFloor);
         
+        // Assert we received move up event for target floor 5
         Assert.Equal(expectedTargetFloor, actualTargetFloor);
     }
 }
